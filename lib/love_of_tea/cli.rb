1269fdf690a215d27c1f855a088429c18967cc8b
avelineamour <razelle@satyaorthopedic.com>
2018-02-26 22:13:59 -0500

    Methods to provide all of each type of tea, price by name method, list teas methods, more CLI input

--- lib/love_of_tea/cli.rb
+++ lib/love_of_tea/cli.rb
@@ -1,6 +1,16 @@
-
+require_relative '../../config/environment.rb'
+require_relative './scraper.rb'
+require_relative './tea.rb'
 
 class LoveOfTea::CLI
+
+  def initialize
+    LoveOfTea::Tea.create_from_collection("https://www.rishi-tea.com/category/green-tea")
+    LoveOfTea::Tea.create_from_collection("https://www.rishi-tea.com/category/caffeine-free-herbal")
+    LoveOfTea::Tea.create_from_collection("https://www.rishi-tea.com/category/white-tea")
+    LoveOfTea::Tea.create_from_collection("https://www.rishi-tea.com/category/black-tea")
+    LoveOfTea::Tea.create_from_collection("https://www.rishi-tea.com/category/chai")
+  end
 
   def call
     menu
@@ -34,7 +44,102 @@ class LoveOfTea::CLI
   end
 
   def match
-    puts "\nMatching you with the perfect tea..."
+    puts "\nWhat sort of effect are you looking for in your tea?\n\n1. Energy\n2. Calming\n3. Warming/Spice\n\n"
+
+    input = gets.strip
+
+    if input == "1"
+      puts "\nPlease choose a caffeine level.\n\n1. Low\n2. Medium\n3. High\n\n"
+
+      input2 = gets.strip
+      if input2 == "1"
+
+        puts "\nWould you like us to select a tea for you? Or would you prefer to choose from a list?\n\n1. Select For Me\n2. Choose My Own\n\n"
+        input3 = gets.strip
+          if input3 == "1"
+
+            randomtea = LoveOfTea::Tea.white.shuffle.first
+            randomteaname = randomtea.name
+            price = LoveOfTea::Tea.price_by_name(randomteaname)
+            description = randomtea.description
+
+            puts "\nWe have chosen the lovely #{randomteaname} for you. #{description}. Depending on quantity, that will cost #{price}. Enjoy!\n"
+          elsif input3 == "2"
+            list_white_tea
+            input4 = gets.strip.to_i
+            tea = LoveOfTea::Tea.white[input4 - 1].name
+            price = LoveOfTea::Tea.white[input4 - 1].price
+              puts "\nExcellent choice. #{description}. Depending on quantity that will cost #{price}. Please enjoy a sample of #{tea} on us!"
+          else puts "Invalid Input"
+            match
+          end
+
+      elsif input2 == "2"
+
+        puts "\nWould you like us to select a tea for you? Or would you prefer to choose from a list?\n\n1. Select for me\n2. Choose My Own"
+        input3 = gets.strip
+          if input3 == "1"
+
+            randomtea = LoveOfTea::Tea.green.shuffle.first
+            randomteaname = randomtea.name
+            price = LoveOfTea::Tea.price_by_name(randomteaname)
+
+            description = randomtea.description
+            puts "\nWe have chosen the lovely #{randomteaname} for you. #{description}. Depending on quantity, that will cost #{price}. Enjoy!\n"
+            
+          elsif input3 == "2"
+            list_green_tea
+            input4 = gets.strip.to_i
+            tea = LoveOfTea::Tea.green[input4 - 1].name
+            price = LoveOfTea::Tea.green[input4 - 1].price
+              puts "\nExcellent choice. #{description}. Depending on quantity that will cost #{price}. Please enjoy a sample of #{tea} on us!"
+          else puts "Invalid Input"
+            match
+          end
+
+      elsif input2 == "3"
+        puts "\n"
+      end
+    elsif input == "2"
+      # Herbals yo
+    elsif input == "3"
+      # Chais mofo
+
+    elsif input != "exit"
+      puts "Invalid input. Please try again."
+      match
+    else
+    end
+  end
+
+  def list_white_tea
+    puts "\n"
+    LoveOfTea::Tea.white.each_with_index do |tea, index| puts "#{index + 1}. #{tea.name}"
+    end
+  end
+
+  def list_green_tea
+    puts "\n"
+    LoveOfTea::Tea.green.each_with_index do |tea, index| puts "#{index + 1}. #{tea.name}"
+    end
+  end
+
+  def list_black_tea
+    puts "\n"
+    LoveOfTea::Tea.black.each_with_index do |tea, index| puts "#{index + 1}. #{tea.name}"
+    end
+  end
+
+  def list_herbal_tea
+    puts "\n"
+    LoveOfTea::Tea.herbal.each_with_index do |tea, index| puts "#{index + 1}. #{tea.name}"
+    end
+  end
+
+  def list_chai_tea
+    puts "\n"
+    LoveOfTea::Tea.chai.each_with_index do |tea, index| puts "#{index + 1}. #{tea.name}"
+    end
   end
 
 end
--- lib/love_of_tea/tea.rb
+++ lib/love_of_tea/tea.rb
@@ -1,8 +1,58 @@
-
+require_relative '../../config/environment.rb'
+require_relative './scraper.rb'
+require_relative './cli.rb'
 
 class LoveOfTea::Tea
-  attr_accessor :type, :description, :price
+  attr_accessor :name, :type, :price, :description, :caffeine
 
-  
+  @@all = []
+
+  def initialize(teahash)
+    @name = teahash[:name]
+    @type = teahash[:type]
+    @price = teahash[:price] ? teahash[:price] : teahash[:price2]
+    @description = teahash[:description]
+    @caffeine = if teahash[:type] == "Green" then "medium" elsif teahash[:type] == "White" then "low" elsif teahash[:type] == "Black" then "high" else nil end
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.create_from_collection(tea_url)
+    LoveOfTea::Scraper.scrape_by_tea_url(tea_url).each do |teahash|
+    self.new(teahash)
+    end
+  end
+
+  def self.green
+    @@all.reject {|tea| tea.type != "Green"}
+  end
+
+  def self.white
+    @@all.reject {|tea| tea.type != "White"}
+  end
+
+  def self.black
+    @@all.reject {|tea| tea.type != "Black"}
+  end
+
+  def self.herbal
+    @@all.reject {|tea| tea.type != "Herbal"}
+  end
+
+  def self.chai
+    @@all.reject {|tea| tea.type != "Chai"}
+  end
+
+  def self.price_by_name(teaname)
+    tea = @@all.detect {|tea| tea.name == teaname}
+    tea.price
+  end
 
 end
